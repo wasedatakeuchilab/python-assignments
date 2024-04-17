e9e0e1522bdf78d49cf729e6e4d167c235f3acf5
Yuto Nakama <yuto_susi_2003@fuji.waseda.jp>
2024-04-17 13:40:03 +0900

    Solve example

--- assignments/20240412/main.py
+++ assignments/20240412/main.py
@@ -1,2 +1,27 @@
 def fizzbuzz(n: int) -> None:
     "Prints n lines of the FizzBuzz game."
+
+    if n < 1:
+        print('InputError, input natural numbers')
+        exit()
+    try:
+        for index in range(1,n+1):
+            if index % 5 == 0 and index % 3 == 0:
+                print('FizzBuzz')
+
+            elif index % 5 == 0:
+                print('Buzz')
+
+            elif index % 3 == 0:
+                print('Fizz')
+
+            else:
+                print(index)
+    except TypeError:
+        print('TypeError, input natural numbers')
+
+if __name__ == '__main__':
+    fizzbuzz(30)
+    fizzbuzz(1)
+    fizzbuzz(5.8)
+    fizzbuzz(-3)
