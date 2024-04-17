341f0356ac61ddefe1e329eb7dd027de0cab3e35
Reiho Ozaki <kumareiho@akane.waseda.jp>
2024-04-17 16:20:29 +0900

    Solove assignment

--- assignments/20240412/main.py
+++ assignments/20240412/main.py
@@ -1,2 +1,10 @@
 def fizzbuzz(n: int) -> None:
-    "Prints n lines of the FizzBuzz game."
+    for i in range(n):
+        if i % 15 == 0:
+            print ("FizzBuzz")
+        elif i % 3 == 0:
+            print ("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else:
+            print(i)
