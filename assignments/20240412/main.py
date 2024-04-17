e91da7de7e7c09daf564c06e51ca3588680bb7d8
Riku Iwasaki <riku.iwasaki@asagi.waseda.jp>
2024-04-17 19:25:54 +0900

    Solve 20240412

--- assignments/20240412/main.py
+++ assignments/20240412/main.py
@@ -1,2 +1,11 @@
 def fizzbuzz(n: int) -> None:
     "Prints n lines of the FizzBuzz game."
+    for i in range(1, n + 1):
+        if (i % 3 == 0) and (i % 5 == 0):
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
