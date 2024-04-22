8f210f875d31c3e5d8a16bec52c65ec1695b6ab1
Shuhei Nitta <huisintheta@gmail.com>
2024-04-22 06:03:40 +0000

    Solve

--- assignments/20240419/main.py
+++ assignments/20240419/main.py
@@ -4,3 +4,10 @@ import functools
 @functools.cache
 def fibo(n: int) -> int:
     """Returns the n-th Fibonacci number."""
+    if n < 0:
+        raise ValueError()
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibo(n - 1) + fibo(n - 2)
