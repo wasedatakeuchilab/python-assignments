85eb2ae9f7436249d35bb67c2212e346f316d231
Riku Iwasaki <riku.iwasaki@asagi.waseda.jp>
2024-04-23 13:00:00 +0900

    Solve 20240419

--- assignments/20240419/main.py
+++ assignments/20240419/main.py
@@ -4,3 +4,10 @@ import functools
 @functools.cache
 def fibo(n: int) -> int:
     """Returns the n-th Fibonacci number."""
+    if n < 0:
+        raise ValueError('n is negative.')
+    
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibo(n - 1) + fibo(n - 2)
